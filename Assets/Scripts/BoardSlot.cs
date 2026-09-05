464ed8557c1ce2aebbb64f7937bae115628e35ad
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2026-09-05 15:44:29 +0900

    파일/폴더 정리

--- Assets/Scripts/BoardSlot.cs
+++ Assets/Scripts/BoardSlot.cs
@@ -27,23 +27,4 @@ public class BoardSlot : MonoBehaviour
         _placedBrick = null;
     }
 
-    // 이전 BoardSlot
-
-    private LineBlock placedBlock;
-
-    public bool IsOccupied => placedBlock != null;
-    public LineBlock PlacedBlock => placedBlock;
-
-    // 블록이 슬롯에 배치됐을 때 호출
-    public void Occupy(LineBlock block)
-    {
-        placedBlock = block;
-    }
-
-    // 블록이 제거됐을 때 호출
-    //public void Clear()
-    //{
-    //    placedBlock = null;
-    //}
-
 }
